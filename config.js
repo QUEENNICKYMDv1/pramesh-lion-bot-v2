f0473805d6ffcd48a6b07348791e75d637f543f4
Pramesh04 <111011537+Pramesh04@users.noreply.github.com>
2023-06-11 12:06:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,14 +10,14 @@ global.email = 'kaveeshapakaya@gmail.com'
 global.github = 'https://github.com'
 global.location = 'india'
 global.gurl = 'https://instagram.com/' // add your username
-global.sudo = process.env.SUDO || '94774071805'
-global.devs = '94774071805';
+global.sudo = process.env.SUDO || '94783037971'
+global.devs = '94783037971';
 global.website = 'https://www.darknero.ga' //wa.me/+94701138827
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://i.ibb.co/fSZkGT2/istockphoto-1387900612-170667a.jpg'
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://i.ibb.co/CMCNByx/Picsart-22-07-03-15-45-00-698.jpg'
 module.exports = {
-  botname: process.env.BOT_NAME || 'PAKAYA',
-  ownername:process.env.OWNER_NAME || 'Kaveesha',
-  sessionName: process.env.SESSION_ID || 'eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiUUZpa0xaallWbUNoYWJ0Y1lCMGZ6T2M4MGJqY3pSMlk0bkN0ZlRmT04wUT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiRVVVdCtzUndERUxSNmEyZjR0MUoxQ0pTR0NFaFVjYmx0N3hFd2UzSTJTaz0ifX0sInNpZ25lZElkZW50aXR5S2V5Ijp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJPS3hmeTBDYzFyUGxnelhwZ0lZYkFTUzJRd29sNk0rVjRaNlk4aUVGSjBjPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJURWE0T0gwakh2Mi9zaWdoQkdYTDAyQjRzOXBJUWhEVm1TS1g5VHUwMUhzPSJ9fSwic2lnbmVkUHJlS2V5Ijp7ImtleVBhaXIiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkNBQjA1c2tzeE0veHhqU2FKTFg4Q1doOW1WVXJpaDRpMS81RlQyYWZCblk9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IitLcy9zaDlvUlAwdkp6T3BjUklWTnlpRmtva20zNkp6SnIvQitxVGNnd2s9In19LCJzaWduYXR1cmUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJlcloyTlplaG9WemhUZEFSWnJPMExBSVZZelo2NU15MVZYd0lVdmtGeWlFd0pyaWo3UmVIN1RVQlB0UzlSQXIveGRORm5aek5IZzR3MVRjbzhUSy9EQT09In0sImtleUlkIjoxfSwicmVnaXN0cmF0aW9uSWQiOjI1MCwiYWR2U2VjcmV0S2V5IjoiaWpXSC9GMTBGK0N4VDJYZzZtM29rSnR6NXlSdkVoVUtMKzk4UDNtMkx6OD0iLCJwcm9jZXNzZWRIaXN0b3J5TWVzc2FnZXMiOltdLCJuZXh0UHJlS2V5SWQiOjMxLCJmaXJzdFVudXBsb2FkZWRQcmVLZXlJZCI6MzEsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDUFRmejQwQkVJU28xNklHR0FVPSIsImFjY291bnRTaWduYXR1cmVLZXkiOiJldU9PcDR2eHJWUGxlRlhOZnZwUWxqY20rSU9VS3FBajNWTUpwYVRZQUI0PSIsImFjY291bnRTaWduYXR1cmUiOiI1bVBwZ05RSVFwT0VhS3QrYkFYM2lFQXY5eHNITy8vUHVZVzZxTUt0MWd3bGJCckNXTGQyVlI0MWhNcHI1dUx5WURMeEVwSUplL09XRFhPRER0NnlCUT09IiwiZGV2aWNlU2lnbmF0dXJlIjoicnFFajlIem1FdFJEYmtuY2xpeGh3cnN4TVRBaWs5S3ZMMHR6S0VJbnhpNUlZd3A3MURVYnhRdmY4MXU2RDdlRkRPc3RSaDdmNXQ2TGNNa0JvbzVKQnc9PSJ9LCJtZSI6eyJpZCI6IjQ0NzQ0MTQzOTc5Mzo2QHMud2hhdHNhcHAubmV0IiwibmFtZSI6IkNZQkVSIFlBS1VaQSJ9LCJzaWduYWxJZGVudGl0aWVzIjpbeyJpZGVudGlmaWVyIjp7Im5hbWUiOiI0NDc0NDE0Mzk3OTM6NkBzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJYcmpqcWVMOGExVDVYaFZ6WDc2VUpZM0p2aURsQ3FnSTkxVENhV2syQUFlIn19XSwicGxhdGZvcm0iOiJzbWJhIiwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNjgzMzQ2NDM4fQ==',
+  botname: process.env.BOT_NAME || '༺❤️༒🦁PRAMESH LION BOT🦁༒❤️༻',
+  ownername:process.env.OWNER_NAME || '𝙥𝙧⃢𝙖𝙢⃪𝙚⃨⃕𝙨𝙝 𝙣𝙞⃢𝙡𝙖𝙠𝙨⃨𝙝⃡⃗𝙖⃝𝙣',
+  sessionName: process.env.SESSION_ID || 'Secktor;;;PollingGeorge',
   author: process.env.PACK_INFO || (";")[0] || 'author',
   auto_read_status : process.env.AUTO_READ_STATUS || 'false',
   packname: process.env.PACK_INFO || (";")[1] || 'author', 
